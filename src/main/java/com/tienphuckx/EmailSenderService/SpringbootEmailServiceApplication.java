353c2b5c80050b1e153eea58681e291f45e490a7
Phuckx97 <tienphuckx@gmail.com>
2024-10-17 18:48:02 +0700

    init project

---
+++ src/main/java/com/tienphuckx/EmailSenderService/SpringbootEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.EmailSenderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienphuckx/EmailSenderService/SpringbootEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.EmailSenderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
